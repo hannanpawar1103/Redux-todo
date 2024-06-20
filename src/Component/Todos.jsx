a17acb8c9127ce1fdbe3d561febd387ea4a95f4f
Pawar Abdul Hannan <hannanpawar1103@gmail.com>
2024-06-20 22:24:40 +0530

    remove todo after times up

--- src/Component/Todos.jsx
+++ src/Component/Todos.jsx
@@ -62,10 +62,18 @@ const Todos = () => {
     const hours = Math.floor(totalSeconds / 3600);
     const minutes = Math.floor((totalSeconds % 3600) / 60);
     const seconds = totalSeconds % 60;
-    return `
+    return function () {
+      if (hours == 0 && minutes == 0 && seconds == 0) {
+        return todos.map((todo) => {
+          dispatch(removeTodo(todo.id))
+        })
+      }else{
+        return `
     ${hours.toString().padStart(2, "0")}:
     ${minutes.toString().padStart(2, "0")}:
     ${seconds.toString().padStart(2, "0")}`;
+      }
+    }
   };
 
   return (    
